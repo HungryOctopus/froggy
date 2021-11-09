df0fc9d350ef9f2f9b257ef42b66d2d92e7c0e86
Senimtra <senimtra@hotmail.com>
2021-11-09 10:47:50 +0100

    add node-sass module + rename .css -> .scss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      Froggy App
-    </div>
-  );
+  return <div className="App">Froggy App</div>;
 }
 
 export default App;
