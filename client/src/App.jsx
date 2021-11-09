0e6109776b628a5d7004a5554055c8752e69d888
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-11-09 22:07:48 +0100

    bootstrap react added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>An app to save frogs and toads</h1>
-    </div>
-  );
-=======
 import "./App.scss";
 
 function App() {
   return <div className="App">Froggy App</div>;
->>>>>>> 04e17b30d275c645989aaf831235d8da7bceb5cc
 }
 
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
->>>>>>> 04e17b30d275c645989aaf831235d8da7bceb5cc
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
   document.getElementById('root')
-=======
-  document.getElementById("root")
->>>>>>> 04e17b30d275c645989aaf831235d8da7bceb5cc
 );
