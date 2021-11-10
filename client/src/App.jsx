79a159d4c16d942e0e09a03657ab1f5310b48984
Senimtra <senimtra@hotmail.com>
2021-11-10 12:06:17 +0100

    create home view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.scss";
+import Home from "./views/Home";
 
 function App() {
-  return <div className="App">Froggy App</div>;
+  return (
+    <div className="App">
+      <h2>Froggy App</h2>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/views/Home.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <h4>Home</h4>
+        <div>Google maps api: event location</div>
+      </>
+    );
+  }
+}
+
+export default Home;
