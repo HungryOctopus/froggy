40867747e8d367a87cd7d61e827ab7caf796c33b
Senimtra <senimtra@hotmail.com>
2021-11-10 13:05:14 +0100

    create signup view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./views/Home";
 import Navbar from "./components/Navbar";
 import Statistics from "./views/Statistics";
 import Calendar from "./views/Calendar";
+import Signup from "./views/Signup";
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Switch>
-          <Route path="/" component={Home} exact />
           <Route path="/statistics" component={Statistics} exact />
           <Route path="/calendar" component={Calendar} exact />
+          <Route path="/signup" component={Signup} exact />
+          <Route path="/" component={Home} exact />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ client/src/views/Signup.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <h5>Signup view</h5>
+        <div>Google maps api: set user pin</div>
+      </>
+    );
+  }
+}
+
+export default Signup;
