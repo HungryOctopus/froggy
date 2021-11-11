0a06143664eb51e2421cdbc223c48f5347c40385
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-11-11 20:35:44 +0100

    Updates to message model

--- server/models/message.js
+++ server/models/message.js
@@ -3,24 +3,36 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  subject: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 140
-  },
-  body: {
-    type: String,
-    trim: true,
-    maxlength: 5000
+  notification: {
+    subject: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 140
+    },
+    body: {
+      type: String,
+      trim: true,
+      maxlength: 5000
+    }
   },
   creator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  recipient: {
+    userIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
   }
 });
 
 const Message = mongoose.model('Message', schema);
 
 module.exports = Message;
+
+// Recipients: default will be a radio button set to 'All',
+// second option is to sent to individual volunteers
