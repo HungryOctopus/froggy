706d805d17fe858790e30393e31bc8520d1e89ea
Senimtra <senimtra@hotmail.com>
2021-11-11 16:09:29 +0100

    create GoogleMapsHome component

---
+++ client/src/components/GoogleMapsHome.jsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+class GoogleMapsHome extends Component {
+  constructor() {
+    super();
+    this.state = {
+      center: {
+        lat: 47.95821,
+        lng: 7.901442,
+      },
+      zoom: 11,
+    };
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: "250px",
+          width: "500px",
+          border: "1px solid dimgrey",
+        }}
+      >
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_GOOGLE_MAPS_API,
+          }}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
+        ></GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default GoogleMapsHome;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import GoogleMapsHome from "../components/GoogleMapsHome";
 
 class Home extends Component {
   constructor() {
@@ -13,6 +14,7 @@ class Home extends Component {
       <>
         <h5>Home view</h5>
         <div>Google maps api: event location</div>
+        <GoogleMapsHome />
       </>
     );
   }
