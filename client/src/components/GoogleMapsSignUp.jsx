4d7a6d552c8e6f899e1dce9fffe11307aa607c3d
Senimtra <senimtra@hotmail.com>
2021-11-12 10:41:26 +0100

    create GoogleMapsSignUp component

---
+++ client/src/components/GoogleMapsSignUp.jsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+class GoogleMapsSignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      center: {
+        lat: 50.0,
+        lng: 10.0,
+      },
+      zoom: 14,
+    };
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: "250px",
+          width: "500px",
+          border: "1px solid dimgrey",
+        }}
+      >
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_GOOGLE_MAPS_API,
+          }}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
+        ></GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default GoogleMapsSignUp;
