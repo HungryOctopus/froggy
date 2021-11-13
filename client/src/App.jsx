083100392314b3cf8eb06eaf5ee80599dd118637
Senimtra <senimtra@hotmail.com>
2021-11-13 17:25:24 +0100

    fix app.scss can't resolve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.scss';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './views/Home';
-import Navbar from './components/Navbar';
-import Statistics from './views/Statistics';
-import Calendar from './views/Calendar';
-import Signup from './views/Signup';
-import Login from './views/Login';
-import TotalCounter from './views/TotalCounter';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./views/Home";
+import Navbar from "./components/Navbar";
+import Statistics from "./views/Statistics";
+import Calendar from "./views/Calendar";
+import Signup from "./views/Signup";
+import Login from "./views/Login";
+import TotalCounter from "./views/TotalCounter";
 
 function App() {
   return (
