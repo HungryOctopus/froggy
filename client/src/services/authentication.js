46dd6a3e63780beab455f63a9550bd82bf9d122e
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-11-14 18:06:02 +0100

    Redirecting volunteer to counter page after successful sign-up

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -2,6 +2,7 @@ import api from './api';
 
 export const signUp = (body) => {
   return api.post('/authentication/sign-up', body).then((response) => {
+    window.location.href = '/counter';
     const data = response.data;
     const user = data.user;
     return user;
