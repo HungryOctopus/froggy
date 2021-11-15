0788b6230db7625e991499e6b4eca45278458166
Senimtra <senimtra@hotmail.com>
2021-11-15 16:01:40 +0100

    rename individualStatistics -> statistics

---
+++ client/src/views/Users.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import GoogleMapsUsermap from "../components/GoogleMapsUsermap";
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <h5>Statistics view</h5>
+        <div>Google maps api: map all users</div>
+        <GoogleMapsUsermap />
+      </>
+    );
+  }
+}
+
+export default Users;
