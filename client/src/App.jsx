19f3407edbbd3d68bdc0a503c9f48020197fabc3
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-11-16 20:42:29 +0100

    Linking AdminMessaging to app jsx to render view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./views/Home";
-import Navbar from "./components/Navbar";
-import Users from "./views/Users";
-import Calendar from "./views/Calendar";
-import Signup from "./views/Signup";
-import Login from "./views/Login";
-import TotalCounter from "./views/TotalCounter";
-import Statistics from "./views/Statistics";
-import Footer from "./components/Footer";
-import { Component } from "react";
-import { signOut, loadAuthenticatedUser } from "./services/authentication";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
+import Navbar from './components/Navbar';
+import Users from './views/Users';
+import Calendar from './views/Calendar';
+import Signup from './views/Signup';
+import Login from './views/Login';
+import TotalCounter from './views/TotalCounter';
+import Statistics from './views/Statistics';
+import Footer from './components/Footer';
+import { Component } from 'react';
+import { signOut, loadAuthenticatedUser } from './services/authentication';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminMessaging from './views/AdminMessaging';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       user: null,
-      loaded: false,
+      loaded: false
     };
   }
 
@@ -55,6 +56,13 @@ class App extends Component {
       <BrowserRouter>
         <Navbar user={this.state.user} onSignOut={this.handleSignOut} />
         <Switch>
+          <ProtectedRoute
+            path="/adminmessaging"
+            redirect="/signup"
+            authorized={!this.state.loaded || this.state.user}
+            component={AdminMessaging}
+            exact
+          />
           <ProtectedRoute
             path="/users"
             redirect="/signup"
