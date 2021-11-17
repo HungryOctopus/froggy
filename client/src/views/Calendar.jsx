ca84ea988f0eeacc6fc0a7138bd291cec46618b5
Senimtra <senimtra@hotmail.com>
2021-11-17 14:58:11 +0100

    add calendar + print clicked date

--- client/src/views/Calendar.jsx
+++ client/src/views/Calendar.jsx
@@ -1,18 +1,35 @@
 import { Component } from "react";
+import CalendarComponent from "react-calendar";
 
 class Calendar extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      value: null,
+    };
   }
 
   componentDidMount() {}
+
+  onUserClick = (event) => {
+    console.log(event);
+    const day = JSON.stringify(event);
+    this.setState({
+      value: day,
+    });
+  };
 
   render() {
     return (
       <>
         <h5>Calendar view</h5>
         <div>react calendar package</div>
+        <CalendarComponent
+          className="calendar"
+          onChange={this.onUserClick}
+          // value={this.state.value}
+        />
+        <div>User clicked day:{this.state.value}</div>
       </>
     );
   }
