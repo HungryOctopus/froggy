fb941e7e0c340e465c2f4ff82e2cf5b7d5c930a8
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-11-17 13:36:28 +0100

    navbar design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,58 +3,80 @@ import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light">
-      <div className="container-fluid">
+    <nav
+      className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
+      id="mainNav"
+    >
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          Catch 'em all
+        </Link>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          MENU <i className="fas fa-bars"></i>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page">
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item mx-0 mx-lg-1">
+              <Link
+                to="/"
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+                aria-current="page"
+              >
                 Home
               </Link>
             </li>
             {props.user && props.user.role === 'admin' && (
-              <li className="nav-item">
-                <Link to="/adminmessaging" className="nav-link">
+              <li className="nav-link py-3 px-0 px-lg-3 rounded">
+                <Link
+                  to="/adminmessaging"
+                  className="nav-link py-3 px-0 px-lg-3 rounded"
+                >
                   Messaging
                 </Link>
               </li>
             )}
-            <li className="nav-item">
-              <Link to="/calendar" className="nav-link">
+            <li className="nav-item mx-0 mx-lg-1">
+              <Link
+                to="/calendar"
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+              >
                 Calendar
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/counter" className="nav-link">
+            <li className="nav-item mx-0 mx-lg-1">
+              <Link
+                to="/counter"
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+              >
                 Counter
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/users" className="nav-link">
+            <li className="nav-item mx-0 mx-lg-1">
+              <Link to="/users" className="nav-link py-3 px-0 px-lg-3 rounded">
                 Users
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/statistics" className="nav-link">
+            <li className="nav-item mx-0 mx-lg-1">
+              <Link
+                to="/statistics"
+                className="nav-link py-3 px-0 px-lg-3 rounded py-3 px-0 px-lg-3 rounded"
+              >
                 Statistics
               </Link>
             </li>
             {(props.user && (
-              <li className="nav-item">
+              <li className="nav-item mx-0 mx-lg-1">
                 <Link
                   to="/login"
-                  className="nav-link"
+                  className="nav-link py-3 px-0 px-lg-3 rounded"
                   onClick={props.onSignOut}
                 >
                   Sign out
@@ -62,13 +84,19 @@ const Navbar = (props) => {
               </li>
             )) || (
               <>
-                <li className="nav-item">
-                  <Link to="/login" className="nav-link">
+                <li className="nav-item mx-0 mx-lg-1">
+                  <Link
+                    to="/login"
+                    className="nav-link py-3 px-0 px-lg-3 rounded"
+                  >
                     Log in
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link to="/signup" className="nav-link">
+                <li className="nav-item mx-0 mx-lg-1">
+                  <Link
+                    to="/signup"
+                    className="nav-link py-3 px-0 px-lg-3 rounded"
+                  >
                     Sign up
                   </Link>
                 </li>
