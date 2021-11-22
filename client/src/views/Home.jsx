9e756778c22aba2c0062f17a2bac5c4633c32517
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-11-22 14:59:57 +0100

    signup style & about text to homepage

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,5 +1,5 @@
-import { Component } from "react";
-import GoogleMapsHome from "../components/GoogleMapsHome";
+import { Component } from 'react';
+import GoogleMapsHome from '../components/GoogleMapsHome';
 
 class Home extends Component {
   constructor() {
@@ -49,6 +49,9 @@ class Home extends Component {
             </p>
           </div>
         </header>
+
+        {/* LOCATION SECTION */}
+
         <section
           className="page-section bg-green text-white pt-3 pb-3 mb-0"
           id="location"
@@ -74,7 +77,56 @@ class Home extends Component {
               </div>
               <div className="divider-custom-line bg-white"></div>
             </div>
-            <GoogleMapsHome />
+            <div>
+              <GoogleMapsHome />
+            </div>
+          </div>
+        </section>
+
+        {/* ABOUT SECTION */}
+
+        <section
+          id="about"
+          className="page-section bg-lgrey text-white pt-3 pb-3 mb-0"
+        >
+          <div className="container-fluid p-0">
+            {/* About section heading */}
+
+            <h1 className="page-section-heading text-center text-uppercase text-green">
+              ABOUT
+            </h1>
+            {/* <!-- Icon Divider--> */}
+            <div className="divider-custom divider-light">
+              <div className="divider-custom-line bg-green"></div>
+              <div className="divider-custom-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="36"
+                  height="36"
+                  fill="#95c60a"
+                  className="bi bi-star-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+              </div>
+              <div className="divider-custom-line bg-green"></div>
+            </div>
+            {/* About section content */}
+            <div className="row pb-5 mb-5">
+              <p className="lead text-dark text-align-center px-5 py-3">
+                The nature conservation working group of the BUND regional
+                association near Freiburg looks after one of the largest, still
+                somewhat intact amphibian populations in southern Baden. To this
+                end, volunteers erect fences on both sides of the road to keep
+                the amphibian safe from the street. In the last 10 years, a
+                total of 3000 to 6000 animals (common toads and grass frogs) are
+                carried across the road every year. We need as many committed
+                helpers as possible who are willing to carry amphibians across
+                the road so that our region continues to have a great diversity
+                of species.
+              </p>
+            </div>
           </div>
         </section>
       </>
--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -90,77 +90,135 @@ class Signup extends Component {
 
   render() {
     return (
-      <>
-        <header className="masthead bg-white text-dark text-center mt-5 pt-5">
-          <div className="container d-flex align-items-center flex-column">
-            <h5>Volunteer signup view</h5>
-            <div style={{ width: '500px' }}>
-              <form
-                onSubmit={this.handleFormSubmission}
-                style={{ display: 'flex', flexDirection: 'column' }}
-              >
-                <label htmlFor="input-firstName">First name</label>
-                <input
-                  id="input-firstName"
-                  type="text"
-                  placeholder="First name"
-                  name="firstName"
-                  value={this.state.firstName}
-                  onChange={this.handleInputChange}
-                />
-                <span style={{ color: 'red' }}>
-                  {this.state.errors['firstName']}
-                </span>
-                <label htmlFor="input-firstName">Last name</label>
-                <input
-                  id="input-secondName"
-                  type="text"
-                  placeholder="Last name"
-                  name="secondName"
-                  value={this.state.secondName}
-                  onChange={this.handleInputChange}
-                />
-                <span style={{ color: 'red' }}>
-                  {this.state.errors['secondName']}
-                </span>
-                <label htmlFor="input-email">Email</label>
-                <input
-                  id="input-email"
-                  type="email"
-                  placeholder="Your Email"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                />
-                <span style={{ color: 'red' }}>
-                  {this.state.errors['email']}
-                </span>
-                <label htmlFor="input-password">Password</label>
-                <input
-                  id="input-password"
-                  type="password"
-                  placeholder="A Secure Password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                />
-                <span style={{ color: 'red' }}>
-                  {this.state.errors['password']}
-                </span>
-                <label htmlFor="user-image-upload">Image</label>
-                <input
-                  id="user-image-upload"
-                  type="file"
-                  name="imageUrl"
-                  onChange={this.handleFileUpload}
-                />
-                <GoogleMapsSignUp onGetCoordinates={this.getUserCoordinates} />
-                <button>Sign Up</button>
-              </form>
+      <div className="contact-form">
+        <div className="container contact-container align-items-center">
+          <div className="d-flex justify-content-center align-items-center">
+            <div className="card contact-card pb-5">
+              <div className="card-header contact-header">
+                <h2>Sign up</h2>
+              </div>
+              <div className="card-body">
+                <form onSubmit={this.handleFormSubmission}>
+                  {/* First name */}
+
+                  <div className="input-group form-group">
+                    <div className="input-group prepend">
+                      <span className="input-group label-text">First name</span>
+                    </div>
+                    <input
+                      id="input-firstName"
+                      type="text"
+                      placeholder="First name"
+                      className="form-control"
+                      name="firstName"
+                      value={this.state.firstName}
+                      onChange={this.handleInputChange}
+                    />
+                    <span style={{ color: 'red' }}>
+                      {this.state.errors['firstName']}
+                    </span>
+                  </div>
+
+                  {/* Last name */}
+
+                  <div className="input-group form-group">
+                    <div className="input-group prepend">
+                      <span className="input-group label-text">Last name</span>
+                    </div>
+                    <input
+                      id="input-secondName"
+                      type="text"
+                      placeholder="Last name"
+                      className="form-control"
+                      name="secondName"
+                      value={this.state.secondName}
+                      onChange={this.handleInputChange}
+                    />
+                    <span style={{ color: 'red' }}>
+                      {this.state.errors['secondName']}
+                    </span>
+                  </div>
+
+                  {/* E-mail */}
+
+                  <div className="input-group form-group">
+                    <div className="input-group prepend">
+                      <span className="input-group label-text">E-mail</span>
+                    </div>
+                    <input
+                      id="input-email"
+                      type="email"
+                      placeholder="Your Email"
+                      className="form-control"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleInputChange}
+                    />
+                    <span style={{ color: 'red' }}>
+                      {this.state.errors['email']}
+                    </span>
+                  </div>
+
+                  {/* Password */}
+
+                  <div className="input-group form-group">
+                    <div className="input-group prepend">
+                      <span className="input-group label-text">Password</span>
+                    </div>
+                    <input
+                      id="input-password"
+                      type="password"
+                      placeholder="A Secure Password"
+                      className="form-control"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleInputChange}
+                    />
+                    <span style={{ color: 'red' }}>
+                      {this.state.errors['password']}
+                    </span>
+                  </div>
+
+                  {/* Choose profile picture */}
+
+                  <div className="input-group form-group">
+                    <div className="input-group prepend">
+                      <span className="input-group label-text">
+                        Choose a profile picture
+                      </span>
+                    </div>
+                    <input
+                      id="user-image-upload"
+                      className="btn"
+                      type="file"
+                      name="imageUrl"
+                      onChange={this.handleFileUpload}
+                    />
+                  </div>
+
+                  {/* Location on map */}
+
+                  <div>
+                    <div className="input-group prepend">
+                      <span className="input-group label-text">
+                        Where are you located?
+                      </span>
+                    </div>
+                    <GoogleMapsSignUp
+                      onGetCoordinates={this.getUserCoordinates}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <button type="submit" className="btn">
+                      Sign Up
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </header>
-      </>
+        </div>
+      </div>
     );
   }
 }
