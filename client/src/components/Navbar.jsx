5449533baf3a51779a7a8b2fcce3c3f0e2d29c7d
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-11-24 19:44:38 +0100

    navbar fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 const Navbar = (props) => {
   return (
     <nav
-      className="navbar navbar-expand-md bg-orange text-uppercase fixed-top"
+      className="navbar navbar-expand-lg bg-orange text-uppercase fixed-top"
       id="mainNav"
     >
       <div className="container">
-        <Link to="/" className="navbar-brand">
-          Catch 'em all
+        <Link to="/" className="navbar-brand ">
+          Froggy
         </Link>
         <button
-          className="navbar-toggler text-uppercase font-weight-bold bg-green rounded"
+          className="navbar-toggler text-uppercase rounded py-2"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarResponsive"
@@ -20,14 +20,28 @@ const Navbar = (props) => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          MENU <i className="fas fa-bars"></i>
+          MENU
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="27"
+            fill="currentColor"
+            className="navbar-toggler-icon"
+            viewBox="0 1 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item mx-0 mx-lg-1">
               <Link
                 to="/"
-                className="nav-link py-3 px-0 px-lg-3 rounded"
+                className="nav-link py-3 px-1 px-lg-3 rounded"
                 aria-current="page"
               >
                 Home
@@ -37,37 +51,46 @@ const Navbar = (props) => {
               <li className="nav-link py-3 px-0 px-lg-3 rounded">
                 <Link
                   to="/adminmessaging"
-                  className="nav-link py-3 px-0 px-lg-3 rounded"
+                  className="nav-link py-3 px-1 px-lg-3 rounded"
                 >
                   Messaging
                 </Link>
               </li>
             )}
-            <li className="nav-item mx-0 mx-lg-1">
-              <Link
-                to="/calendar"
-                className="nav-link py-3 px-0 px-lg-3 rounded"
-              >
-                Calendar
-              </Link>
-            </li>
-            <li className="nav-item mx-0 mx-lg-1">
-              <Link
-                to="/counter"
-                className="nav-link py-3 px-0 px-lg-3 rounded"
-              >
-                Counter
-              </Link>
-            </li>
-            <li className="nav-item mx-0 mx-lg-1">
-              <Link to="/users" className="nav-link py-3 px-0 px-lg-3 rounded">
-                Users
-              </Link>
-            </li>
+            {props.user && (
+              <li className="nav-item mx-0 mx-lg-1">
+                <Link
+                  to="/calendar"
+                  className="nav-link py-3 px-1 px-lg-3 rounded"
+                >
+                  Calendar
+                </Link>
+              </li>
+            )}
+            {props.user && (
+              <li className="nav-item mx-0 mx-lg-1">
+                <Link
+                  to="/counter"
+                  className="nav-link py-3 px-1 px-lg-3 rounded"
+                >
+                  Counter
+                </Link>
+              </li>
+            )}
+            {props.user && (
+              <li className="nav-item mx-0 mx-lg-1">
+                <Link
+                  to="/users"
+                  className="nav-link py-3 px-1 px-lg-3 rounded"
+                >
+                  Users
+                </Link>
+              </li>
+            )}
             <li className="nav-item mx-0 mx-lg-1">
               <Link
                 to="/statistics"
-                className="nav-link py-3 px-0 px-lg-3 rounded py-3 px-0 px-lg-3 rounded"
+                className="nav-link py-3 px-1 px-lg-3 rounded py-3 px-0 px-lg-3 rounded"
               >
                 Statistics
               </Link>
@@ -76,7 +99,7 @@ const Navbar = (props) => {
               <li className="nav-item mx-0 mx-lg-1">
                 <Link
                   to="/login"
-                  className="nav-link py-3 px-0 px-lg-3 rounded"
+                  className="nav-link py-3 px-1 px-lg-3 rounded"
                   onClick={props.onSignOut}
                 >
                   Sign out
@@ -87,7 +110,7 @@ const Navbar = (props) => {
                 <li className="nav-item mx-0 mx-lg-1">
                   <Link
                     to="/login"
-                    className="nav-link py-3 px-0 px-lg-3 rounded"
+                    className="nav-link py-3 px-1 px-lg-3 rounded"
                   >
                     Log in
                   </Link>
@@ -95,7 +118,7 @@ const Navbar = (props) => {
                 <li className="nav-item mx-0 mx-lg-1">
                   <Link
                     to="/signup"
-                    className="nav-link py-3 px-0 px-lg-3 rounded"
+                    className="nav-link py-3 px-1 px-lg-3 rounded"
                   >
                     Sign up
                   </Link>
