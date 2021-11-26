56ec3374d9c85dfcb5d974cda862848d19635b27
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-11-27 00:26:54 +0100

    styling of the statistic page

--- client/src/views/Statistics.jsx
+++ client/src/views/Statistics.jsx
@@ -1,17 +1,17 @@
-import { Component } from "react";
+import { Component } from 'react';
 
-import ChartBar from "../components/ChartBar";
-import ChartPie from "../components/ChartPie";
-import ChartLine from "../components/ChartLine";
-import ChartTotal from "../components/ChartTotal";
+import ChartBar from '../components/ChartBar';
+import ChartPie from '../components/ChartPie';
+import ChartLine from '../components/ChartLine';
+import ChartTotal from '../components/ChartTotal';
 
 import {
   totalCountState,
   typesUserState,
   userDailyState,
   animalTypesState,
-  monthlyCatchState,
-} from "./../services/chart-states";
+  monthlyCatchState
+} from './../services/chart-states';
 
 import {
   getAllStats,
@@ -21,8 +21,8 @@ import {
   setTotalCount,
   setMonthlyCount,
   setUserDailies,
-  setUserTypes,
-} from "./../services/statistics";
+  setUserTypes
+} from './../services/statistics';
 
 class Statistics extends Component {
   constructor(props) {
@@ -32,7 +32,7 @@ class Statistics extends Component {
       chartDataIndividual: typesUserState(),
       chartDataDaily: userDailyState(),
       chartDataAll: animalTypesState(),
-      chartDataMonthlyCount: monthlyCatchState(),
+      chartDataMonthlyCount: monthlyCatchState()
     };
   }
 
@@ -71,18 +71,37 @@ class Statistics extends Component {
   render() {
     return (
       <>
-        <div className="container-fluid container-stats mt-4">
+        <section className="py-5 mt-5 header text-center bg-green">
+          <div className="container mt-5 text-white py-3">
+            <h1 className="page-section-heading text-center text-uppercase text-white">
+              Your statistics
+            </h1>
+            {/* <!-- Icon Divider--> */}
+            <div className="divider-custom divider-light">
+              <div className="divider-custom-line bg-white"></div>
+              <div className="divider-custom-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="36"
+                  height="36"
+                  fill="currentColor"
+                  class="bi bi-bar-chart-line-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2z" />
+                </svg>
+              </div>
+              <div className="divider-custom-line bg-white"></div>
+            </div>
+          </div>
+        </section>
+
+        <div className="container-fluid container-stats">
           <div className="row row-stats">
             <div className="col stats-col">
               <div className="card stats-card">
-                <h2>Total amount of saved animals:</h2>
-                <ChartTotal chartData={this.state.chartDataTotal} />
-              </div>
-            </div>
-            <div className="col stats-col">
-              <div className="card stats-card">
-                <h2>
-                  Animal types saved by{" "}
+                <h2 className="stats-title">
+                  Animal types saved by{' '}
                   {this.props.user && this.props.user.firstName}:
                 </h2>
                 <ChartBar chartData={this.state.chartDataIndividual} />
@@ -90,22 +109,65 @@ class Statistics extends Component {
             </div>
             <div className="col stats-col">
               <div className="card stats-card">
-                <h2>
+                <h2 className="stats-title">
                   {this.props.user && this.props.user.firstName}'s daily
                   catches:
                 </h2>
                 <ChartLine chartData={this.state.chartDataDaily} />
               </div>
             </div>
+          </div>
+        </div>
+
+        <section className="py-5 header text-center bg-green">
+          <div className="container text-white py-3">
+            <h1 className="page-section-heading text-center text-uppercase text-white">
+              Statistics of the group
+            </h1>
+            {/* <!-- Icon Divider--> */}
+            <div className="divider-custom divider-light">
+              <div className="divider-custom-line bg-white"></div>
+              <div className="divider-custom-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="36"
+                  height="36"
+                  fill="currentColor"
+                  class="bi bi-pie-chart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z" />
+                </svg>
+              </div>
+              <div className="divider-custom-line bg-white"></div>
+            </div>
+          </div>
+        </section>
+
+        <div className="container-fluid container-stats">
+          <div className="row row-stats">
             <div className="col stats-col">
               <div className="card stats-card">
-                <h2>Group's monthly amount of saved animals:</h2>
+                <h2 className="stats-title">
+                  Group's monthly amount of saved animals:
+                </h2>
                 <ChartBar chartData={this.state.chartDataMonthlyCount} />
               </div>
             </div>
+
             <div className="col stats-col">
               <div className="card stats-card">
-                <h2>Animal types saved by the group:</h2>
+                <h2 className="stats-title">Total amount of saved animals:</h2>
+                <ChartTotal chartData={this.state.chartDataTotal} />
+              </div>
+            </div>
+          </div>
+          <div className="row row-stats">
+            <div className="col stats-col">
+              <div className="card stats-card">
+                <h2 className="stats-title">
+                  Animal types saved by the group:
+                </h2>
                 <ChartPie chartData={this.state.chartDataAll} />
               </div>
             </div>
